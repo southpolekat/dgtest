9bf3655a2c4cb4a56c92e49c22d5caaf66b6fba0
d895075 <kat.liu@team.telstra.com>
2019-03-04 12:12:05 +0800

    stop xdrive

--- xdrive_fs.sh
+++ xdrive_fs.sh
@@ -15,16 +15,17 @@ name = "local_csv"
 argv = ["xdr_fs/xdr_fs", "csv", "/tmp/data"]
 EOF
 
+xdrctl stop /tmp/xdrive.toml
 xdrctl deploy /tmp/xdrive.toml
 xdrctl start /tmp/xdrive.toml
 
 gpssh -h sdw1 'mkdir -p /tmp/data'
 gpssh -h sdw2 'mkdir -p /tmp/data'
 
-gpssh -h sdw1 'echo "1,1.99" > /tmp/data/xdrive_1.csv' 
-gpssh -h sdw1 'echo "2,2.99" > /tmp/data/xdrive_2.csv'
-gpssh -h sdw2 'echo "3,3.99" > /tmp/data/xdrive_3.csv' 
-gpssh -h sdw2 'echo "4,4.99" > /tmp/data/xdrive_4.csv'
+gpssh -h sdw1 'echo "1,1.99" | tee /tmp/data/xdrive_1.csv' 
+gpssh -h sdw1 'echo "2,2.99" | tee /tmp/data/xdrive_2.csv'
+gpssh -h sdw2 'echo "3,3.99" | tee /tmp/data/xdrive_3.csv' 
+gpssh -h sdw2 'echo "4,4.99" | tee /tmp/data/xdrive_4.csv'
 
 psql -d $db << EOF
 CREATE EXTERNAL TABLE tt_r
