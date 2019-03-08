bb328684dfaf34f96ba964ddc63add6578b8e073
kat <southpolekat@yahoo.com.hk>
2019-03-08 20:12:29 +1100

    add comments for gpcrondump

--- utilities/gpcrondump_gpdbrestore.sh
+++ utilities/gpcrondump_gpdbrestore.sh
@@ -15,12 +15,21 @@ select count(*), sum(i), avg(i) from tt;
 END
 
 gpcrondump -a -C --dump-stats -g -G -h -r -x $db
+### -a : no prompt
+### -C : clean catalog before restore
+### -g : copy config files
+### -G : dump global objects
+### -h : record dump details
+### -r : rollback on failure
 
 psql -d $db -c "select * from gpcrondump_history ;"
 
 dropdb $db
 
 gpdbrestore -a -e -s $db
+### -a : no prompt
+### -e : create target database before restore
+### -s database_name
 
 psql $db -c "select count(*), sum(i), avg(i) from tt;"
 
