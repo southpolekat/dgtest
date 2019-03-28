095cacfcf18ad47c5324678acae90f57d34adaa8
kat <southpolekat@yahoo.com.hk>
2019-03-29 08:41:43 +1100

    Lock information where LockType = Relation

--- others/check_locks.sql
+++ others/check_locks.sql
@@ -10,3 +10,23 @@ from pg_locks l
 where not l.granted
 group by 1,2,3;
 
+-- Lock information where LockType = "Relation"
+select
+    d.datname,
+    c.relname,
+    l.locktype,
+    l.pid,
+    l.mode,
+    s.usename,
+    s.query_start,
+    s.current_query
+from
+    pg_locks l,
+    pg_stat_activity s,
+    pg_database d,
+    pg_class c
+where
+    l.database = d.oid and
+    l.pid = s.procpid and
+    l.relation = c.oid and
+    not l.granted
