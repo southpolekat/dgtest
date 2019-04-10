d79de2853c4f51f31b41ab72a1ec7835622d5935
kat <southpolekat@yahoo.com>
2019-04-11 02:51:16 +1000

    update

---
+++ others/tensorflow_transducer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Reference: http://vitessedata.com/blog/tensorflow_transducer/
+
+# install python 3 and tensorflow on centos 7.5
+#   sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+#   sudo yum update -y
+#   sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
+#   python3.6 -V
+#   sudo pip3.6 install tensorflow
