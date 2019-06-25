1d7c5f7d9214b3788247b151b254dc1365743585
wkliu <wkliu@MacBook-Pro-4.local>
2019-06-25 10:50:29 +0800

    test vd = 0 on numeric

--- features/fast_decimal.sh
+++ features/fast_decimal.sh
@@ -8,6 +8,8 @@ dg setup -decimal $db
 
 psql -a -d $db << EOF 
 
+set vitesse.enable=1;
+
 create table tt (
 	i	integer,
 	f	double precision,
@@ -25,12 +27,17 @@ insert into tt select
 	(i+0.123)::numeric(15,3)
 from generate_series(1,1000000) i;
 
+select count(*) from tt;
+
 \timing on
 
-select avg(f), sum(f) from tt;
-select avg(n), sum(n) from tt;
-select avg(d64), sum(d64) from tt;
-select avg(d128), sum(d128) from tt;
+select avg(f), sum(2*f) from tt;
+select avg(d64), sum(2*d64) from tt;
+select avg(d128), sum(2*d128) from tt;
+select avg(n), sum(2*n) from tt;
+
+set vitesse.enable=0;
+select avg(n), sum(2*n) from tt;
 
 EOF
 
