7e2586b131493639e17e0f4cb790f4cd951eb623
Kat <southpolekat@yahoo.com.hk>
2019-06-25 11:07:22 +0800

    hostname variables

--- xdrive/xdrive_fs.sh
+++ xdrive/xdrive_fs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# hostname of sdw1 and sdw2
+sdw1=sdw1
+sdw2=sdw2
+
 db=dgtest$$
 createdb $db
 
@@ -8,25 +12,25 @@ cat <<END > /tmp/xdrive.toml
 [xdrive]
 dir = "/tmp/xdrive"
 port = 7171 
-host = ["sdw1", "sdw2"]
+host = ["$sdw1", "$sdw2"]
 
 [[xdrive.mount]]
 name = "local_csv"
 argv = ["xdr_fs/xdr_fs", "csv", "/tmp/data"]
 END
 
-gpssh -h sdw1 'pkill -9 xdrive'
-gpssh -h sdw2 'pkill -9 xdrive'
+gpssh -h $sdw1 'pkill -9 xdrive'
+gpssh -h $sdw2 'pkill -9 xdrive'
 
 xdrctl deploy /tmp/xdrive.toml
 xdrctl start /tmp/xdrive.toml
 gpssh -f ~/hostfile pidof xdrive
 
 gpssh -f ~/hostfile 'mkdir -p /tmp/data'
-gpssh -h sdw1 'echo "1,1.99" | tee /tmp/data/xdrive_1.csv' 
-gpssh -h sdw1 'echo "2,2.99" | tee /tmp/data/xdrive_2.csv'
-gpssh -h sdw2 'echo "1,1.99" | tee /tmp/data/xdrive_1.csv'
-gpssh -h sdw2 'echo "2,2.99" | tee /tmp/data/xdrive_2.csv'
+gpssh -h $sdw1 'echo "1,1.99" | tee /tmp/data/xdrive_1.csv' 
+gpssh -h $sdw1 'echo "2,2.99" | tee /tmp/data/xdrive_2.csv'
+gpssh -h $sdw2 'echo "1,1.99" | tee /tmp/data/xdrive_1.csv'
+gpssh -h $sdw2 'echo "2,2.99" | tee /tmp/data/xdrive_2.csv'
 
 psql -d $db << END
 CREATE EXTERNAL TABLE tt_r
