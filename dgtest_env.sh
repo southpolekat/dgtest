5b6b4f63affa7d7eb7086dbfc5a5cb3017d11d8a
southpolekat <southpolekat@yahoo.com.hk>
2020-09-15 05:34:07 +0000

    add common env variable

---
+++ dgtest_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+db_host=localhost
+db_port=5432
+db_name=dgtest
+db_user=gpadmin
+db_schema=public
+db_table=dgtest_tt
+
+loftd_path=/tmp/loftdata
+loftd_port=8787
+
+ver=$(psql -t -c "show vitesse.version" | cut -f4 -d ' ' | cut -f1 -d '.')
+echo $ver
