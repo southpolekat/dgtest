86fe41499f12ed8c06d8b6458d9c4a3568747ac7
southpolekat <southpolekat@yahoo.com.hk>
2020-09-22 04:20:03 +0000

    add variables

--- dgtest_env.sh
+++ dgtest_env.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# hostname of segment 1 and segment 2
+sdw1=sdw1
+sdw2=sdw2
+
 db_host=localhost
 db_port=5432
 db_name=dgtest
@@ -20,8 +24,10 @@ loftd_host2=mdw
 loftd_path2=/tmp/loftdata2
 loftd_port2=8788
 
-# hostname of segment 1 and segment 2
-sdw1=sdw1
-sdw2=sdw2
+xdrive_port=7171
+xdrive_path=/tmp/xdrive
+
+s3pool_port=12345
+s3pool_path=/tmp/s3pool
 
 ver=$(psql -t -c "show vitesse.version" | cut -f4 -d ' ' | cut -f1 -d '.')
