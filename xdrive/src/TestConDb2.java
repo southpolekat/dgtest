f938ee8ec95ffc443e058c735fe8214f762dde15
southpolekat <southpolekat@yahoo.com.hk>
2020-10-15 12:17:53 +0000

    test db2

---
+++ xdrive/src/TestConDb2.java
@@ -0,0 +1,17 @@
+import java.sql.*;  
+class TestConDb2{  
+   public static void main(String args[]){  
+      try {  
+         Class.forName("com.ibm.db2.jcc.DB2Driver");  
+
+         Connection con=DriverManager.getConnection(  
+            "jdbc:db2://my_db2:50000/test_db","db2inst1","test_passwd");  
+
+         Statement stmt=con.createStatement();  
+         ResultSet rs=stmt.executeQuery("select * from test_table");  
+         while(rs.next())  
+            System.out.println(rs.getInt(1));  
+         con.close();  
+       } catch(Exception e){ System.out.println(e);}  
+   }  
+}  
--- xdrive/src/test.sh
+++ xdrive/src/test.sh
@@ -8,4 +8,8 @@ echo test oracle jdbc connection
 javac TestConOracle.java
 java -cp .:/home/gpadmin/ojdbc8.jar TestConOracle
 
+echo test DB2 jdbc connection
+javac TestConDb2.java
+java -cp .:/home/gpadmin/db2jcc-db2jcc4.jar TestConDb2
+
 rm *.class
