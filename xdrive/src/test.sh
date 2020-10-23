037560f5ee5deacfd4ccb727b2ca1ed32f93de60
southpolekat <southpolekat@yahoo.com.hk>
2020-10-23 13:32:46 +0000

    use case

--- xdrive/src/test.sh
+++ xdrive/src/test.sh
@@ -1,19 +1,28 @@
 #!/bin/bash 
 
-echo test Postgres jdbc connection
-javac TestConPostgres.java
-java -cp .:/home/gpadmin/postgresql-42.2.1.jar TestConPostgres
+db=${1:-all}
 
-echo test mysql jdbc connection
-javac TestConMysql.java
-java -cp .:/usr/share/java/mysql.jar TestConMysql
-
-echo test oracle jdbc connection
-javac TestConOracle.java
-java -cp .:/home/gpadmin/ojdbc8.jar TestConOracle
-
-echo test DB2 jdbc connection
-javac TestConDb2.java
-java -cp .:/home/gpadmin/db2jcc-db2jcc4.jar TestConDb2
+case $db in 
+	postgres | all )
+		echo test Postgres jdbc connection
+		javac TestConPostgres.java
+		java -cp .:/home/gpadmin/postgresql-42.2.1.jar TestConPostgres
+		;;
+	mysql | all )
+		echo test mysql jdbc connection
+		javac TestConMysql.java
+		java -cp .:/usr/share/java/mysql.jar TestConMysql
+		;;
+	oracle | all )
+		echo test oracle jdbc connection
+		javac TestConOracle.java
+		java -cp .:/home/gpadmin/ojdbc8.jar TestConOracle
+		;;
+	db2 | all )
+		echo test DB2 jdbc connection
+		javac TestConDb2.java
+		java -cp .:/home/gpadmin/db2jcc-db2jcc4.jar TestConDb2
+		;;
+esac
 
 rm *.class
