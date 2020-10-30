a879d0419a0636b2660db5783479c477d98e7992
southpolekat <southpolekat@yahoo.com.hk>
2020-10-30 15:08:49 +0000

    test string_agg

--- functions/string_functions.sh
+++ functions/string_functions.sh
@@ -4,15 +4,18 @@ set -e
 
 source ../dgtest_env.sh
 
-[ $ver -eq "16" ] && exit
-
-# Install LEFT and RIGHT function
-
-psql -d ${db_name} -f ~/deepgreendb/share/postgresql/contrib/dgx.sql
-
+dglog Test string_agg
 psql -a -d ${db_name} << END
+CREATE TEMP table tmp (t text) distributed randomly;
+INSERT INTO tmp values ('a'), ('b'), ('c');
+SELECT string_agg(t) from tmp;
+SELECT string_agg(t,'|') from tmp;
+END
 
+dglog Test LEFT and RIGHT
+[ $ver -eq "16" ] && exit
+psql -d ${db_name} -f ~/deepgreendb/share/postgresql/contrib/dgx.sql
+psql -a -d ${db_name} << END
 SELECT right('abcde',3);
 SELECT left('abcde',3); 
-
 END
