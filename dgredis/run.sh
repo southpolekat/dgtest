ec549203072334ecc186f2cd2bb405a28eaa1cd9
Kat <southpolekat@yahoo.com.hk>
2021-05-04 03:51:10 +0000

    test with redis-cli

--- dgredis/run.sh
+++ dgredis/run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # sudo apt install redis-server
 
-PGDATABSE=tpch dgredis dgredis.toml
+PGDATABASE=tpch dgredis dgredis.toml
+
+echo "GET s:active" | redis-cli -h redis1
+echo "GET s:timestamp" | redis-cli -h redis1
+echo "LRANGE c:table 0 -1" |  redis-cli -h redis1
+echo "GET t:orders:1" | redis-cli -h redis1
 
 dgredis_get -h redis1:6379 "t:orders:1"
