fd24ef4d6dc21da904ffd682c616a26a8799fa5e
Kat <southpolekat@yahoo.com.hk>
2021-05-04 04:00:37 +0000

    test tpch.region

--- dgredis/run.sh
+++ dgredis/run.sh
@@ -6,9 +6,12 @@ set -e
 
 PGDATABASE=tpch dgredis dgredis.toml
 
+echo "GET s:config" | redis-cli -h redis1
 echo "GET s:active" | redis-cli -h redis1
 echo "GET s:timestamp" | redis-cli -h redis1
 echo "LRANGE c:table 0 -1" |  redis-cli -h redis1
-echo "GET t:orders:1" | redis-cli -h redis1
+echo "GET t:region:1" | redis-cli -h redis1
+echo "HGET c:t:region ncol"  | redis-cli -h redis1
+echo "HGET c:t:region col:1"  | redis-cli -h redis1
 
-dgredis_get -h redis1:6379 "t:orders:1"
+dgredis_get -h redis1:6379 "t:region:1"
